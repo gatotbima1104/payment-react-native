f41eae1c270bd9ac0a2fe326f50211e458a0b8b3
gatotbima1104 <gatotbima2002@gmail.com>
2024-02-17 21:42:36 +0700

    add: main-page

--- src/components/ItemDonation.tsx
+++ src/components/ItemDonation.tsx
@@ -1,20 +1,21 @@
-import { View, Text, Image } from "react-native"
+import { View, Text, Image, ImageSourcePropType } from "react-native"
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 interface IItemDonationProps {
-    uriImage: string;
+    uriImage: ImageSourcePropType;
     name: string;
     price: number;
+    titleBadge?: string;
 }
 
-const ItemDonation: React.FC<IItemDonationProps> = ({ uriImage, name, price }) => {
+const ItemDonation: React.FC<IItemDonationProps> = ({ uriImage, name, price, titleBadge }) => {
     return (
-        <View className="mt-5 p-2 rounded " style={{ width: wp(35) }}>
+        <View className="mt-5 p-2 rounded space-y-1" style={{ width: wp(40) }}>
             <View className="bg-slate-200 rounded">
-                <View className="bg-green-700 rounded-full p-2 w-2/3">
-                    <Text className="text-white font-bold text-center" style={{fontSize: wp(2.5)}}>Environment</Text>
+                <View className="bg-green-700 rounded-full p-2 w-2/3 absolute z-10 top-1">
+                    <Text className="text-white font-bold text-center" style={{ fontSize: wp(2.5) }}>{titleBadge}</Text>
                 </View>
-                <Image source={{ uri: uriImage }} style={{ width: wp(30), height: hp(10) }} className="border" />
+                <Image source={uriImage} style={{ width: wp(35), height: hp(15), objectFit: 'cover' }} className="rounded" />
             </View>
             <Text className="font-bold tracking-wide" style={{ fontSize: 15 }}>{name.length > 13 ? name.slice(0, 13) + ('...') : name}</Text>
             <Text className="text-blue-500">${price}</Text>
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import { FlatList, Pressable, Text, View, Image } from "react-native";
+import { FlatList, Pressable, Text, View, Image, ScrollView } from "react-native";
 import { HandThumbUpIcon } from "react-native-heroicons/outline";
 import Header from "../components/Header";
 import SearchBar from "../components/SearchBar";
@@ -10,14 +10,22 @@ import ItemDonation from "../components/ItemDonation";
 const Home = ({ navigation }: any) => {
     return (
         <SafeAreaView className="flex-1 mx-4 mt-5 space-y-10">
-            {/* Header */}
-            <Header greeting="Hello," icon={HandThumbUpIcon} name="Bobby Kartanegara" />
+            <ScrollView>
 
-            {/* Search bar  */}
-            <SearchBar placeholder="Search for anything" onSearch={(value)=> console.log(value)}/>
+                {/* Header */}
+                <Header greeting="Hello," icon={HandThumbUpIcon} name="Bobby Kartanegara" />
 
-            {/* Item Donation */}
-            <ItemDonation name="Tree Cactus Images" uriImage={require('../../assets/img.png')} price={49}/>
+                {/* Search bar  */}
+                <SearchBar placeholder="Search for anything" onSearch={(value) => console.log(value)} />
+
+                {/* Item Donation */}
+                <View className="flex-row flex-wrap justify-between mx-2">
+                    <ItemDonation name="Tree Cactus Images" uriImage={require('../../assets/cactus.jpeg')} price={49} titleBadge={'lifestyle'} />
+                    <ItemDonation name="Tree Cactus Images" uriImage={require('../../assets/cactus.jpeg')} price={49} titleBadge={'lifestyle'} />
+                    <ItemDonation name="Tree Cactus Images" uriImage={require('../../assets/cactus.jpeg')} price={49} titleBadge={'lifestyle'} />
+                    <ItemDonation name="Tree Cactus Images" uriImage={require('../../assets/cactus.jpeg')} price={49} titleBadge={'lifestyle'} />
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 
